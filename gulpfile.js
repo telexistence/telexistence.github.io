e41ae21bcfc0bc1f799e85c26adc5a7d6e7a3e95
Toshiya Nakakura <nakakura@me.com>
2016-01-04 17:41:25 +0900

    gulp update

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,12 @@ var usemin = require('gulp-usemin');
 var uglify = require('gulp-uglify');
 var rev = require('gulp-rev');
 var bower = require('gulp-bower');
+var runSequence = require('run-sequence');
+
+gulp.task('default', function(callback){
+    return runSequence(
+        'bower', 'usemin', callback);
+});
 
 gulp.task('bower', function() {
     return bower({ directory: 'src/bower_components' });
