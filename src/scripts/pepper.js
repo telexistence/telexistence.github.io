1e3187c36c088234e0f492b53fd703e1f8419b8e
Toshiya Nakakura <nakakura@me.com>
2016-03-24 13:24:12 +0900

    googleIo

--- src/scripts/pepper.js
+++ src/scripts/pepper.js
@@ -19,7 +19,7 @@ var TexCardBoard;
             this.network = new TexCardBoard.Network(peerId);
             this.android = new TexCardBoard.AndroidDevice();
             this.android.on(TexCardBoard.AndroidDevice.OnDeviceOrientation, function (e) {
-                _this.network.send(e);
+                _this.network.append(e);
             });
             this.network.on(TexCardBoard.Network.onVideo, function (stream) {
                 var video = document.getElementById('video');
